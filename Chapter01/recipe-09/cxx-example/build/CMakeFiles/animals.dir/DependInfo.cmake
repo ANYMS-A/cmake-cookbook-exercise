fbd9b5ce5d63f3bdb3cba364b22d8e3067b29b0b
allancodeman <allancodeman@163.com>
2021-04-06 13:49:37 +0800

    add chapter01-recipe-09

---
+++ Chapter01/recipe-09/cxx-example/build/CMakeFiles/animals.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/Animal.cpp" "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/build/CMakeFiles/animals.dir/Animal.cpp.o"
+  "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/Cat.cpp" "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/build/CMakeFiles/animals.dir/Cat.cpp.o"
+  "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/Dog.cpp" "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-01/recipe-09/cxx-example/build/CMakeFiles/animals.dir/Dog.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "animals_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

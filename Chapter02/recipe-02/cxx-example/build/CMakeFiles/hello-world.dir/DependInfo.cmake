2c119e609bcc587181a53a3ce64f76379f749353
allancodeman <allancodeman@163.com>
2021-04-10 20:23:03 +0800

    add chapter02-recipe-02

---
+++ Chapter02/recipe-02/cxx-example/build/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-02/recipe-02/cxx-example/hello-world.cpp" "/Users/crissallan/CLionProjects/cmake-cookbook/chapter-02/recipe-02/cxx-example/build/CMakeFiles/hello-world.dir/hello-world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IS_MACOS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
